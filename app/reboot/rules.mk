821f8672e382424a392d12a05d48581c0e99c0b3
Mis012 <michael.srba@seznam.cz>
2019-08-05 15:36:27 +0200

    [app][reboot] - move `boot_linux_from_ext2` out of reboot.c; use `boot_linux_from_ext2` to boot selected boot entry

--- app/reboot/rules.mk
+++ app/reboot/rules.mk
@@ -9,6 +9,7 @@ MODULES += \
 OBJS += \
 	$(LOCAL_DIR)/gfx.o \
 	$(LOCAL_DIR)/reboot.o \
-	$(LOCAL_DIR)/menu.o \
+	$(LOCAL_DIR)/boot.o \
 	$(LOCAL_DIR)/config.o \
+	$(LOCAL_DIR)/menu.o \
 	$(LOCAL_DIR)/fs_util.o
