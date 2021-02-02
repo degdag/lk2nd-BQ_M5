0745a0dc161f7f67fcba46655633e1f4e572c7ce
Degdag Mohamed <degdagmohamed@gmail.com>
2021-02-02 19:58:42 +0100

    Update rules.mk

--- dts/msm8916/rules.mk
+++ dts/msm8916/rules.mk
@@ -25,6 +25,6 @@ DTBS += \
 	$(LOCAL_DIR)/msm8929-samsung-r00.dtb \
 	$(LOCAL_DIR)/msm8929-samsung-r04.dtb \
 	$(LOCAL_DIR)/msm8939-asus-z00t.dtb \
-	$(LOCAL_DIR)/msm8939-huawei-kiwi.dtb \
 	$(LOCAL_DIR)/msm8939-mtp.dtb \
-	$(LOCAL_DIR)/msm8939-qrd-skuk.dtb
+	$(LOCAL_DIR)/msm8939-qrd-skuk.dtb \
+	$(LOCAL_DIR)/msm8939-bq-piccolo.dtb
